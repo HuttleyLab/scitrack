618ca13d108f3cc94221543b16e10dbb2410081a
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2016-08-19 09:22:28 +1000

    fixed license classifier in setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
             "Intended Audience :: Science/Research",
             "Programming Language :: Python :: 2.7",
             "Programming Language :: Python :: 3",
-            "License :: OSI Approved :: BSD-3-Clause",
+            "License :: OSI Approved :: BSD License",
             "Topic :: Scientific/Engineering :: Bio-Informatics",
             "Topic :: Software Development :: Libraries :: Python Modules",
             "Operating System :: OS Independent",
