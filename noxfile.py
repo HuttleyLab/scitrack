2f2e3bcf35179c5799d43077f689b6211fdfb4bf
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2024-10-08 09:43:51 +1100

    BUG: address failure on python 3.13, fixes #21
    [CHANGED] hookup 3.13 in github actions

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 import nox
 
 
-@nox.session(python=[f"3.{v}" for v in range(9, 13)])
+@nox.session(python=[f"3.{v}" for v in range(9, 14)])
 def test(session):
     session.install(".[test]")
     session.chdir("tests")
