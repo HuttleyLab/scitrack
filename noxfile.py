c68aecc5ad5ce669b34655404abb1227ce3834cc
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2025-08-20 09:18:08 +1000

    DEV: support py3.10 and up

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 import nox
 
 
-@nox.session(python=[f"3.{v}" for v in range(9, 14)])
+@nox.session(python=[f"3.{v}" for v in range(10, 14)])
 def test(session):
     session.install(".[test]")
     session.chdir("tests")
