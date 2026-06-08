f8431f522145f516a2a7fa6070a400971391f34f
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2026-06-08 12:26:18 +1000

    DEV: use uv for project management

--- noxfile.py
+++ noxfile.py
@@ -1,9 +1,18 @@
 import nox
 
+py_vers = [f"3.{v}" for v in range(10, 15)]
 
-@nox.session(python=[f"3.{v}" for v in range(10, 14)])
+
+@nox.session(python=py_vers[-1], venv_backend="uv")
+def fmt(session: nox.Session) -> None:
+    session.install("-e", ".", "--group", "dev")
+    session.run("ruff", "check", "--fix-only", ".")
+    session.run("ruff", "format", ".")
+
+
+@nox.session(python=py_vers, venv_backend="uv")
 def test(session):
-    session.install(".[test]")
+    session.install("-e", ".", "--group", "dev")
     session.chdir("tests")
     session.run(
         "pytest",
